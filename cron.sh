17bdd3f63e0c20ad4a1ebb2f0f6a3b6b01b98de8
Christian Budde Christensen <budde377@gmail.com>
2017-04-30 15:38:15 +0200

    ref: Indentation and stuff

--- cron.sh
+++ cron.sh
@@ -15,26 +15,29 @@ sleep 10
 IFS=';' read -ra HOSTA <<< "$HOSTS"
 if [ $# -eq 0 ]; then
   certbot renew
-else
-  for host in "${HOSTA[@]}"; do
+fi
+
+for host in "${HOSTA[@]}"; do
+  if [ $# -eq 0 ]; then
+    echo "Not fetching cert"
+  else
     certbot certonly --force-renewal --webroot -w /var/www/ --agree-tos --email budde@laundree.io -d "$host"
-    fi
-    fileFullChain="/etc/letsencrypt/live/$host/fullchain.pem"
-    filePrivateKey="/etc/letsencrypt/live/$host/privkey.pem"
-    if [[ -f $fileFullChain && -f $filePrivateKey ]]; then
-      echo "Moving certificate"
-      cat ${fileFullChain} ${filePrivateKey} > "/var/certs/$host.pem"
-    else
-      echo "Certificates files not found. Will not move."
-    fi
-  done
-fi 
+  fi
+  fileFullChain="/etc/letsencrypt/live/$host/fullchain.pem"
+  filePrivateKey="/etc/letsencrypt/live/$host/privkey.pem"
+  if [[ -f $fileFullChain && -f $filePrivateKey ]]; then
+    echo "Moving certificate"
+    cat ${fileFullChain} ${filePrivateKey} > "/var/certs/$host.pem"
+  else
+    echo "Certificates files not found. Will not move."
+  fi
+done
+ 
 
 echo "Killing server"
 kill %%
 
-if [ -n "$TRIGGER" ]
-    then
-        echo "Triggering"
-        curl -X POST "$TRIGGER"
+if [ -n "$TRIGGER" ]; then
+  echo "Triggering"
+  curl -X POST "$TRIGGER"
 fi
