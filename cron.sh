27bdea219916df5adcde2cccb1b0356bf80358ec
Christian Budde Christensen <budde377@gmail.com>
2017-04-30 15:36:15 +0200

    Fix: Add sleep and use renew command properly

--- cron.sh
+++ cron.sh
@@ -10,22 +10,25 @@ cd /var/www
 echo "Starting server"
 python -m SimpleHTTPServer 80 &
 
-IFS=';' read -ra HOSTA <<< "$HOSTS"
+sleep 10
 
-for host in "${HOSTA[@]}"; do
-  if [ $# -eq 0 ]
-  then certbot renew --force-renewal -d "$host"
-  else certbot certonly --force-renewal --webroot -w /var/www/ --agree-tos --email budde@laundree.io -d "$host"
-  fi
-  fileFullChain="/etc/letsencrypt/live/$host/fullchain.pem"
-  filePrivateKey="/etc/letsencrypt/live/$host/privkey.pem"
-  if [[ -f $fileFullChain && -f $filePrivateKey ]]; then
-    echo "Moving certificate"
-    cat ${fileFullChain} ${filePrivateKey} > "/var/certs/$host.pem"
-  else
-    echo "Certificates files not found. Will not move."
-  fi
-done
+IFS=';' read -ra HOSTA <<< "$HOSTS"
+if [ $# -eq 0 ]; then
+  certbot renew
+else
+  for host in "${HOSTA[@]}"; do
+    certbot certonly --force-renewal --webroot -w /var/www/ --agree-tos --email budde@laundree.io -d "$host"
+    fi
+    fileFullChain="/etc/letsencrypt/live/$host/fullchain.pem"
+    filePrivateKey="/etc/letsencrypt/live/$host/privkey.pem"
+    if [[ -f $fileFullChain && -f $filePrivateKey ]]; then
+      echo "Moving certificate"
+      cat ${fileFullChain} ${filePrivateKey} > "/var/certs/$host.pem"
+    else
+      echo "Certificates files not found. Will not move."
+    fi
+  done
+fi 
 
 echo "Killing server"
 kill %%
